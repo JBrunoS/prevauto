e709c1df680bb7e4ca2c647c42e5cc90399ac80d
João Bruno <j_bruno_s@hotmail.com>
2019-04-24 15:10:16 -0300

    conexao de banco de dados

---
+++ model/conexao.php
@@ -0,0 +1,35 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$db = "prevauto";
+
+$mysqli = new mysqli($host, $usuario, $senha, $db);
+$conexao = mysqli_connect("localhost", "root", "", "prevauto");
+
+if ($mysqli->connect_errno) {
+	echo "Erro";
+}
+
+
+?>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+
+	$conn = new PDO("mysql:host=$servername;dbname=prevauto", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	#echo "Deu certo";
+	
+} catch (Exception $e) {
+	echo "Erro ao conectar: " . $e->getMessage();
+}
+
+
+?>
